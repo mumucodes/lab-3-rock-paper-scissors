5583b6914c08f378b5acde066510c4e6914242a8
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-03-26 16:16:59 -0700

    most viable product v1.0

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ let playerChoosesPaper = document.getElementById('paperRadioButton');
 let playerChoosesScissors = document.getElementById('scissorRadioButton');
 let gamePlay = document.getElementById('pressToPlay');
 let resultsDisplay = document.getElementById('computerVisualState');
+let draws = document.getElementById('draws');
+let wins = document.getElementById('wins');
+let losses = document.getElementById('losses')
+console.log(losses);
+let gameWin = 0;
+let gameLoss = 0;
+let gameTie = 0;
 
 gamePlay.addEventListener('click', () => {
     const currentChecked = document.querySelector('input:checked');
@@ -17,17 +24,35 @@ gamePlay.addEventListener('click', () => {
     // alert(result)
     
     if ( result === 'you lose')  {
+        gameLoss++
         resultsDisplay.textContent = 'sorry not today'
+        losses.textContent = `Losses: ${gameLoss}`;
+
     }
     else if ( result === 'you win') {
+        gameWin++
         resultsDisplay.textContent = 'winner winner chicken dinner!'
+        wins.textContent = `Wins: ${gameWin}`;
     }
     else if ( result === 'draw') {
+        gameTie++
         resultsDisplay.textContent = 'looks like we need to go again'
-    }
-        
+        draws.textContent = `Ties: ${gameTie}`;
 
-console.log(resultsDisplay);
+    }
+
+    
+    
+    const counter = document.querySelector('input:checked');
+
+
+    if (counter === 'you lose') {
+        winsVsLoss.value === 1 + 'my score is (winsVsLoss.value)'
+    }
+
+    console.log(counter);
+
+
 });
 // i still need to display the gameresults into a field on the screen
 // i still need to display a counter with the wins and losses
